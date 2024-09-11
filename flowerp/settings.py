355fa99938d0f4ce0c9714261305094e85812ef6
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2024-09-11 09:43:23 -0300

    refactor: change view redirection from update to create

--- flowerp/settings.py
+++ flowerp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-laxxpx4xyb!2)3pzo93p1=3zqh8)tj31&u%8o06#f1n=7#0fm=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.100']
 
 
 # Application definition
