873ff0960c11f23746a458d2a1ff471dd3285859
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2024-09-11 13:57:02 -0300

    refactor: makes the identification_number field of the Enterprise model unique

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Enterprise(models.Model):
     corporate_reason = models.CharField(max_length=80)
     trade_name = models.CharField(max_length=80)
-    identification_number = models.CharField(max_length=14)
+    identification_number = models.CharField(max_length=14, unique=True)
 
     def __str__(self) -> str:
         return str(self.trade_name)
